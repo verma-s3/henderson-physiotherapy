19a8115553ee8c85e787d084469fe274c2114874
sonia <sonia@psone.ca>
2025-01-29 17:07:31 -0700

    workign on about us page

--- wp-content/themes/henderson/static/js/app.js
+++ wp-content/themes/henderson/static/js/app.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function($) {
-  // fixedHeaderOnScroll();
   $('#toggle').click(function() {
     $(this).toggleClass('active');
     $('header').toggleClass('header-color');
@@ -19,61 +18,23 @@ jQuery(document).ready(function($) {
     //     disable: 'mobile',
     //     once: true
     // });
-   
-  // //*** Smooth Scroll ***
-  //   $(function() {
-  //     $('a[href*="#"]:not([href="#"])').click(function() {
-  //       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-  //         var target = $(this.hash);
-  //         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-  //         if (target.length) {
-  //           $('html, body').animate({
-  //             scrollTop: target.offset().top - 70
-  //           }, 1000);
-  //           return false;
-  //         }
-  //       }
-  //     });
-  //   });//End Smooth Scroll
+
+      $(".team-content .read-more").click(function () {
+          var $this = $(this);
+          var $moreText = $this.prev().find(".more-text");
   
-  //*** Fixed header ***
-  // function fixedHeaderOnScroll(){
-  //   // var heroHeight = window.innerHeight; //use me if want to display fixed header according to innerHeight.
-  //   // if ($(this).scrollTop() >= heroHeight) {// 
-  //   if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-  //       $('header').addClass('fixed-header');    // Fade in the arrow
-  //   } else {
-  //       $('header').removeClass('fixed-header');   // Else fade out the arrow
-  //   }
-  // }
-  // $(window).scroll(function () {
-  //   fixedHeaderOnScroll();
-  // });
-
-  /************************************************
-  uncomment follwoing code if window.innerheight is
-  included in fixedHeaderOnScroll function.  
-  *************************************************/ 
-
-  // $(window).resize(function() {
-  //   fixedHeaderOnScroll();
-  // });
-
-
-  //*** Scroll to Top *** use with less *** use with html ***
-    // $(window).scroll(function() {
-    //     if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
-    //         $('#return-to-top').fadeIn(200);    // Fade in the arrow
-    //     } else {
-    //         $('#return-to-top').fadeOut(200);   // Else fade out the arrow
-    //     }
-    // });
+          if ($moreText.is(":visible")) {
+              $moreText.slideUp();
+              $this.html("Read More");
+          } else {
+              $moreText.slideDown();
+              $this.html("Read Less");
+          }
+      });
   
-    // $('#return-to-top').click(function() {      // When arrow is clicked
-    //     $('body,html').animate({
-    //         scrollTop : 0                       // Scroll to top of body
-    //     }, 500);
-    // });//End Scroll to Top
+ 
+
+
   
 
   
@@ -93,49 +54,57 @@ jQuery(document).ready(function($) {
 
 
   //Slick SLider
-  // $('.slider').slick({
-  //   dots: false,
-  //   centerMode: true,
-  //   infinite: true,
-  //   arrows: true,
-  //   slidesToShow: 4.67,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1500,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2,
-  //         infinite: true,
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         slidesToShow: 2,
-  //         slidesToScroll: 2
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     }
-  //     // You can unslick at a given breakpoint now by adding:
-  //     // settings: "unslick"
-  //     // instead of a settings object
-  //   ]
-  // });
+  $('.gallery-slider').slick({
+    dots: false,
+    infinite: false,
+    arrows: false,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 1000,
+    // fade: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 1500,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+  });
 
   
 });
   
+$('[data-fancybox="galleryImages"]').fancybox({
+  buttons: [
+    "slideShow",
+    "thumbs",
+    "zoom",
+    "fullScreen",
+    "share",
+    "close"
+  ],
+  loop: false,
+  protect: true
+});
